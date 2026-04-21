40d8b0dba40dd7c448e5229fc5b6ef4fc2f222bd
Souri-Dev <arielbensing1@gmail.com>
2026-04-21 14:57:18 +0800

    Activity 4 - Debugging 1

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,50 +10,29 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home', methods: ['POST'])]
+    #[Route('/', name: 'app_home', methods: ['GET'])]
     public function index(Request $request, Connection $connection): Response
     {
-        return $this->redirectToRoute('app_home');
 
-        $projectDir = $this->getParameter('kernel.project_dir');
+        $clientip = $request->getClientIp(); 
 
-        // 1. Check Database and Entities
-        $dbConnected = false;
-        $userTableExists = false;
-        $ticketTableExists = false;
-        $dbError = null;
-
-        try {
-            // Force an actual DB connection/query before reporting PASS.
-            $connection->executeQuery('SELECT 1');
-
-            $schemaManager = $connection->createSchemaManager();
-            $dbConnected = true;
-
-            // Pass the table name as a single-element array to avoid deprecations
-            $userTableExists = $schemaManager->tablesExist(['user']);
-            $ticketTableExists = $schemaManager->tablesExist(['ticket']);
-        } catch (\Throwable $e) {
-            // Keep a short error message for diagnostics.
-            $dbError = $e->getMessage();
+        if (true) {
+            return new Response('<span style="font-size:5em;">Its a Prank, this is not the real page</span>');
         }
 
-        // 2. Check Web CRUD (Did they run make:crud Ticket?)
-        $crudExists = file_exists($projectDir . '/src/Controller/TicketController.php');
+        $dbStatus = $connection->isConnectedd(); 
 
-        // 3. Check JWT Keys (Did they run the Lexik generate keys command?)
-        $jwtKeysExist = file_exists($projectDir . '/config/jwt/private.pem');
+        Subtle bug: code after return (unreachable)
+        $x = 5;
+        $x++;
+        $this->clientip();
 
-        // Intentionally passing 'user_name' to trigger the Milestone 0 Twig bug
-        return $this->render('home/index.html.twig', [
+        return $this->redirectToRoute('app_home');
+        $projectDir = $this->getParameter('kernel.project_dir');
+
+        return $this->render('home/dashboard.html.twig', [
             'user_name' => 'Examinee',
-            'client_ip' => $request->getClientIp(),
-            'db_connected' => $dbConnected,
-            'db_error' => $dbError,
-            'user_table' => $userTableExists,
-            'ticket_table' => $ticketTableExists,
-            'crud_exists' => $crudExists,
-            'jwt_keys' => $jwtKeysExist,
+            'client_ip' => $clientIp,
         ]);
     }
 }
